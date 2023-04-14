c0a0b07960d40d2545a0905fd8724fa12a26237b
Egor Novoselov <hey.egor@icloud.com>
2023-04-14 13:54:19 +0300

    complete webpack setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './scss/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React Webpack App!</h1>
+    </div>
+  );
+}
+
+export default App;
