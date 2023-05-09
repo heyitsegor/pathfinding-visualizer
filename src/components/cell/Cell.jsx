7af3075464d0b1fa8a31e385e81fd13f170dcf92
Egor Novoselov <hey.egor@icloud.com>
2023-05-09 21:38:23 +0300

    fix grammar

--- src/components/cell/Cell.jsx
+++ src/components/cell/Cell.jsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
 
+/*
+TODO: 1. add a way to insert only one start and end points
+      2. change on click event handler to be able to set
+         walls while moving mouse with LMB pressed
+      3. try to optimize Cell component (Current complexity is 6.2)
+*/
+
 export default function Cell({ index }) {
-  const [isWall, setIsWall] = useState('');
+  const [cellType, setCellType] = useState('');
+  // const [isDragging, setIsDragging] = useState(false);
 
   const handleClick = () => {
-    setIsWall(isWall === '' ? 'grid__wall' : '');
+    setCellType(cellType === '' ? 'grid__cell--wall' : '');
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 32) {
+      setCellType(cellType === '' ? 'grid__cell--wall' : '');
+    }
   };
 
   return (
     <div
-      className={`grid__cell ${isWall}`}
+      className={`grid__cell ${cellType}`}
       onClick={handleClick}
+      onKeyDown={handleKeyDown}
+      aria-hidden="true"
+      // onMouseDown={() => setIsDragging(true)}
+      // onMouseUp={() => setIsDragging(false)}
+      // onMouseMove={handleMouseMove}
       data-col={`${index}`}
     />
   );
--- src/components/toolbar/Toolbar.jsx
+++ src/components/toolbar/Toolbar.jsx
@@ -5,6 +5,7 @@ export default function Toolbar() {
     <div className="toolbar">
       <div className="toolbar__item toolbar__selectAlgorithm">Select Algorithm</div>
       <div className="toolbar__item toolbar__selectSpeed">Select Speed</div>
+      <div className="toolbar__item toolbar__clearBoard">Clear Board</div>
     </div>
   );
 }
